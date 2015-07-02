005ca60fec37b444de4c710e7c8b8e470ff8ebcd
Pocket <p0ck31@gmail.com>
2015-07-02 15:46:40 -0400

    Push to github! Comments go to firebase but no tree

--- scripts/services/comment.js
+++ scripts/services/comment.js
@@ -9,6 +9,7 @@ app.factory('Comment', function(FURL, $firebase, Auth) {
     all: comments,
 
     getCommentarray: function(questionId) {
+      console.log("QuestionID getCommentarray: " + questionId );
       var resCom = $firebase(ref.child('comments').child(questionId)).$asObject();
       return resCom;
       // var resResult = $firebase(ref.child('questions').child(questionId)).$asObject();
@@ -16,10 +17,14 @@ app.factory('Comment', function(FURL, $firebase, Auth) {
       // do stuff to get comments for a question
     },
 
-    createComment: function(questionId, comment){
-      individualCommentarray = getCommentarray(questionId);
+    createComment: function(comment){
       comment.datetime = Firebase.ServerValue.TIMESTAMP;
-      return individualCommentarray.$add(comment);
+      return comments.$add(comment);
+
+      // individualCommentarray = this.getCommentarray(questionId);
+      // comment.datetime = Firebase.ServerValue.TIMESTAMP;
+      // return individualCommentarray.$add(comment);
+
       // do stuff to create a comment
     },
 
